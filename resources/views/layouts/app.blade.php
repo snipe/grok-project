fc03bb45a18c3d4d60186591cdccf3ab41945240
snipe <snipe@snipe.net>
2018-06-09 12:48:01 -0700

    Removed unused css/js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,8 @@
     <title>{{ config('app.name') }}</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,300i,400,400i,500,500i,600,600i,700,700i&amp;subset=latin-ext">
+
+    <link href="/css/app.css" rel="stylesheet" />
     <script src="/js/require.min.js"></script>
     <script>
         requirejs.config({
@@ -30,11 +32,10 @@
     <!-- c3.js Charts Plugin -->
     <link href="/plugins/charts-c3/plugin.css" rel="stylesheet" />
     <script src="/plugins/charts-c3/plugin.js"></script>
-    <!-- Google Maps Plugin -->
-    <link href="/plugins/maps-google/plugin.css" rel="stylesheet" />
-    <script src="/plugins/maps-google/plugin.js"></script>
     <!-- Input Mask Plugin -->
     <script src="/plugins/input-mask/plugin.js"></script>
+
+    <script src="/js/app.js"></script>
 </head>
 <body class="">
 <div class="page">
