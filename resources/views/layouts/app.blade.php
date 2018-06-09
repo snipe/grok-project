a84dcb6b1b0fad12071d26c28d759d43d0844de1
snipe <snipe@snipe.net>
2018-06-09 11:20:20 -0700

    Updated nav, fixed logout link

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -120,9 +120,14 @@
                                 <a class="dropdown-item" href="#">
                                     <i class="dropdown-icon fe fe-help-circle"></i> Need help?
                                 </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="dropdown-icon fe fe-log-out"></i> Sign out
-                                </a>
+                                <a class="dropdown-item" href="{{ route('logout') }}"
+                                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    <i class="dropdown-icon fe fe-log-out"></i>
+                                    {{ __('Logout') }}</a>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
                             </div>
                         </div>
                     </div>
@@ -151,53 +156,19 @@
                     <div class="col-lg order-lg-first">
                         <ul class="nav nav-tabs border-0 flex-column flex-lg-row">
                             <li class="nav-item">
-                                <a href="./index.html" class="nav-link"><i class="fe fe-home"></i> Home</a>
+                                <a href="{{ route('home') }}" class="nav-link"><i class="fe fe-home"></i> Home</a>
                             </li>
                             <li class="nav-item">
-                                <a href="javascript:void(0)" class="nav-link" data-toggle="dropdown"><i class="fe fe-box"></i> Interface</a>
+                                <a href="javascript:void(0)" class="nav-link" data-toggle="dropdown"><i class="fe fe-box"></i> Projects</a>
                                 <div class="dropdown-menu dropdown-menu-arrow">
-                                    <a href="./cards.html" class="dropdown-item ">Cards design</a>
-                                    <a href="./charts.html" class="dropdown-item ">Charts</a>
-                                    <a href="./pricing-cards.html" class="dropdown-item ">Pricing cards</a>
+                                    @foreach (Auth::user()->projects as $user_projects)
+                                        <a class="dropdown-item" href="{{ route('project.show', ['id' => $user_projects->id ]) }}">{{ $user_projects->name }}</a>
+                                    @endforeach
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Create a Project</a>
                                 </div>
                             </li>
-                            <li class="nav-item dropdown">
-                                <a href="javascript:void(0)" class="nav-link" data-toggle="dropdown"><i class="fe fe-calendar"></i> Components</a>
-                                <div class="dropdown-menu dropdown-menu-arrow">
-                                    <a href="./maps.html" class="dropdown-item ">Maps</a>
-                                    <a href="./icons.html" class="dropdown-item ">Icons</a>
-                                    <a href="./store.html" class="dropdown-item ">Store</a>
-                                    <a href="./blog.html" class="dropdown-item ">Blog</a>
-                                    <a href="./carousel.html" class="dropdown-item ">Carousel</a>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a href="javascript:void(0)" class="nav-link active" data-toggle="dropdown"><i class="fe fe-file"></i> Pages</a>
-                                <div class="dropdown-menu dropdown-menu-arrow">
-                                    <a href="./profile.html" class="dropdown-item ">Profile</a>
-                                    <a href="./login.html" class="dropdown-item ">Login</a>
-                                    <a href="./register.html" class="dropdown-item ">Register</a>
-                                    <a href="./forgot-password.html" class="dropdown-item ">Forgot password</a>
-                                    <a href="./400.html" class="dropdown-item ">400 error</a>
-                                    <a href="./401.html" class="dropdown-item ">401 error</a>
-                                    <a href="./403.html" class="dropdown-item ">403 error</a>
-                                    <a href="./404.html" class="dropdown-item ">404 error</a>
-                                    <a href="./500.html" class="dropdown-item ">500 error</a>
-                                    <a href="./503.html" class="dropdown-item ">503 error</a>
-                                    <a href="./email.html" class="dropdown-item ">Email</a>
-                                    <a href="./empty.html" class="dropdown-item active">Empty page</a>
-                                    <a href="./rtl.html" class="dropdown-item ">RTL mode</a>
-                                </div>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a href="./form-elements.html" class="nav-link"><i class="fe fe-check-square"></i> Forms</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="./gallery.html" class="nav-link"><i class="fe fe-image"></i> Gallery</a>
-                            </li>
-                            <li class="nav-item">
-                                <a href="./docs/index.html" class="nav-link"><i class="fe fe-file-text"></i> Documentation</a>
-                            </li>
+
                         </ul>
                     </div>
                 </div>
