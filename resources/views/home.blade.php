32f76eecc0dc85bf626911142d5f11c3e88e5aea
snipe <snipe@snipe.net>
2018-06-09 02:03:27 -0700

    List companies, projects

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,35 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
+            <div id="mute"></div>
+            <div id="app"></div>
+
+
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    @if ($companies->count() > 0)
+                        <table class="table">
+                            @foreach($companies as $company)
+                                <tr>
+                                    <td>
+                                        <a href="{{ route('company.show', ['id' => $company->id]) }}">{{ $company->name }}</a>
+                                    </td>
+                                    <td>
+                                        @foreach($company->projects as $project)
+                                            <a href="{{ route('project.show', ['id' => $project->id]) }}">{{ $project->name }} </a>
+                                        @endforeach
+                                    </td>
 
-                    You are logged in!
+                                </tr>
+                            @endforeach
+
+                        </table>
+
+                    @else
+                        You don't have any companies set up yet.
+                    @endif
                 </div>
             </div>
         </div>
