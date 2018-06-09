84d85d46f81a01351363a61138af342aaf2b1f8c
snipe <snipe@snipe.net>
2018-06-09 12:47:30 -0700

    Add masks to login form

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,9 +10,18 @@
 
                     <!-- email -->
 
-                    <div class="form-group">
-                        <label for="email" class="form-label">{{ __('E-Mail Address') }}</label>
+                <!-- email -->
+                <div class="form-group">
+                    <label for="email" class="form-label">
+                        {{ __('E-Mail Address') }}
+                        <span class="form-required">*</span>
+                    </label>
+                    <div class="input-icon">
+                        <span class="input-icon-addon">
+                          <i class="fe fe-user"></i>
+                        </span>
                         <input id="email" type="email" class="form-control{{ $errors->has('email') ? '  is-invalid state-invalid' : '' }}" name="email" value="{{ old('email') }}" placeholder="Enter email" autofocus>
+                    </div>
 
                         @if ($errors->has('email'))
                             <div class="invalid-feedback"> {{ $errors->first('email') }} </div>
@@ -25,7 +34,12 @@
                         <label for="password" class="form-label">{{ __('Password') }}
                             <a href="{{ route('password.request') }}" class="float-right small">{{ __('Forgot Your Password?') }}</a>
                         </label>
+                        <div class="input-icon">
+                        <span class="input-icon-addon">
+                          <i class="fe fe-lock"></i>
+                        </span>
                         <input id="password" type="password" class="form-control{{ $errors->has('password') ? '  is-invalid state-invalid' : '' }}" name="password" value="{{ old('password') }}" autofocus>
+                        </div>
 
                         @if ($errors->has('password'))
                             <div class="invalid-feedback"> {{ $errors->first('password') }} </div>
@@ -34,18 +48,26 @@
 
                 <div class="form-group">
                     <label class="custom-control custom-checkbox">
+                        <input type="checkbox" class="custom-control-input" />
+                        <span class="custom-control-label">Remember me</span>
+                    </label>
+                </div>
+
+
+                <div class="form-group">
+                    <label class="custom-control custom-checkbox">
                         <input type="checkbox" class="custom-control-input" name="remember" {{ old('remember') ? 'checked' : '' }}>
-                        <span class="custom-control-label">{{ __('Remember Me') }}</span>
+                        <span class="custom-control-label" style="padding-left: 30px;">{{ __('Remember Me') }}</span>
                     </label>
                 </div>
 
                 <div class="form-footer">
-                    <button type="submit" class="btn btn-primary btn-block">Sign in</button>
+                    <button type="submit" class="btn btn-primary btn-block">{{ __('Sign In') }}</button>
                 </div>
             </div>
         </form>
         <div class="text-center text-muted">
-            Don't have account yet? <a href="{{ route('register') }}">Sign up</a>
+            {{ __('Don\'t have an account yet?') }} <a href="{{ route('register') }}">{{ __('Sign Up') }}</a>
 
         </div>
     </div>
