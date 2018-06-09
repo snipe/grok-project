9fe6f0ed83532f3664def42cdea7e4ee71f30877
snipe <snipe@snipe.net>
2018-06-08 19:30:43 -0700

    Updated temp home

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ config('app.name') }}
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://twitter.com/snipeyhead">@snipeyhead</a>
+                    <a href="https://github.com/snipe/grok-project">GitHub</a>
                 </div>
             </div>
         </div>
