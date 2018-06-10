a4887b1df12ca0c1bfc0eab67229e73bc067dbd0
snipe <snipe@snipe.net>
2018-06-10 08:38:34 -0700

    Use root paths so JS doesn’t break in subdirs

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -9,16 +9,16 @@ shim: {
 'core': ['bootstrap', 'jquery'],
 },
 paths: {
-'core': './js/core',
-'jquery': './js/vendors/jquery-3.2.1.min',
-'bootstrap': './js/vendors/bootstrap.bundle.min',
-'sparkline': './js/vendors/jquery.sparkline.min',
-'selectize': './js/vendors/selectize.min',
-'tablesorter': './js/vendors/jquery.tablesorter.min',
-'vector-map': './js/vendors/jquery-jvectormap-2.0.3.min',
-'vector-map-de': './js/vendors/jquery-jvectormap-de-merc',
-'vector-map-world': './js/vendors/jquery-jvectormap-world-mill',
-'circle-progress': './js/vendors/circle-progress.min',
+'core': '/js/core',
+'jquery': '/js/vendors/jquery-3.2.1.min',
+'bootstrap': '/js/vendors/bootstrap.bundle.min',
+'sparkline': '/js/vendors/jquery.sparkline.min',
+'selectize': '/js/vendors/selectize.min',
+'tablesorter': '/js/vendors/jquery.tablesorter.min',
+'vector-map': '/js/vendors/jquery-jvectormap-2.0.3.min',
+'vector-map-de': '/js/vendors/jquery-jvectormap-de-merc',
+'vector-map-world': '/js/vendors/jquery-jvectormap-world-mill',
+'circle-progress': '/js/vendors/circle-progress.min',
 }
 });
 window.tabler = {
