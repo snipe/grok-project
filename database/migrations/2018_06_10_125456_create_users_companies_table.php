6b3b279a069d52f14e33cd7caa82558e3d186906
snipe <snipe@snipe.net>
2018-06-10 08:35:25 -0700

    Add company users tables

---
+++ database/migrations/2018_06_10_125456_create_users_companies_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+
+        Schema::create('users_companies_invites', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('invited_email');
+            $table->string('invited_name');
+            $table->integer('company_id');
+            $table->string('invite_token');
+            $table->string('invite_role');
+            $table->dateTime('accepted_at');
+            $table->timestamps();
+        });
+
+        Schema::create('users_companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->integer('company_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_companies_invites');
+        Schema::dropIfExists('users_companies');
+    }
+}
